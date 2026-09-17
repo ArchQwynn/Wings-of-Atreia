7b1628eced3352574e8e20c401b42535954f4b2b
ArchQwynn <zafraclark@gmail.com>
2026-09-18 02:57:23 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = "woa-pwa-20260916-8";
-const RUNTIME_CACHE = "woa-runtime-20260916-8";
+const CACHE_VERSION = "woa-pwa-20260918-9";
+const RUNTIME_CACHE = "woa-runtime-20260918-9";
 const PRECACHE = [
   "index.html",
   "offline.html",
